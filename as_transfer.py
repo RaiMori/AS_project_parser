eacf4d561cf7712957e8609ea2faad63c136fb8f
raimori <mosyurchak@gmail.com>
2021-03-17 00:11:46 +0100

    add transfer module file

---
+++ as_transfer.py
@@ -0,0 +1,20 @@
+class PIL_File:
+    def __init__(self):
+        pass
+
+    def get_pvi_parameters(self):
+        pass
+
+    def generate_pil(self):
+        pass
+
+
+class RUC_Transfer:
+    def __init__(self):
+        pass
+
+    def generate_pil(self):
+        pass
+
+    def transfer(self):
+        pass
