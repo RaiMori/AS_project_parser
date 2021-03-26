1b312cc3c0fd7987f499167f3bfdcdd0c5d324cc
raimori <mosyurchak@gmail.com>
2021-03-26 21:27:09 +0100

    add as base class

---
+++ automation_studio.py
@@ -0,0 +1,36 @@
+
+
+
+class AutomationStudio:
+
+    def __init__(self):
+        self.inst_path = ''
+        self.global_br_path = ''
+        self.upgrades_path = ''
+
+    def get_as_installations(self):
+        pass
+
+    def get_installed_modules(self):
+        pass
+
+    def download_modules(self):
+        pass
+
+    def install_module(self):
+        pass
+
+    def get_gcc_path(self):
+        pass
+
+    def get_builder_path(self):
+        pass
+
+    def get_pvi_path(self):
+        pass
+
+
+class SDM:
+
+    def __init__(self):
+        pass
