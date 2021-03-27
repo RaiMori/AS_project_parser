fe1f1e4503e7fc599556afe895ddefc5c714c417
raimori <mosyurchak@gmail.com>
2021-03-27 17:05:28 +0100

    type annotations for cpupkg

--- cpupkg.py
+++ cpupkg.py
@@ -1,5 +1,5 @@
 from lxml import etree
-
+from typing import List, Tuple
 
 # HW Configuration managment
 class PhysicalParser:
@@ -9,9 +9,19 @@ class PhysicalParser:
 class PhysicalCfgPkgParser:
     """ Get CPU folder for current hw configuration """
 
+    def __init__(self, physical_path):
+        self.path = physical_path
+
+    def _read_hw_configs(self):
+        pass
+
+    def get_hw_configs(self) -> List:
+        pass
+
 
 class CPUPkgParser:
     ns = "http://br-automation.co.at/AS/Cpu"
+
     def __init__(self, cpu_pkg_path):
         self.path = cpu_pkg_path
         self.cpu_pkg_tree = etree.parse(cpu_pkg_path)
@@ -33,32 +43,37 @@ class CPUPkgParser:
         if isinstance(val, str):
             self._get_runtime_version_node().attrib["Version"] = val
             self.cpu_pkg_tree.write(open(self.path, "wb"), pretty_print=True)
-            #etree.write(self.path, etree.tostring(self.cpu_pkg_tree, pretty_print=True))
+            # etree.write(self.path, etree.tostring(self.cpu_pkg_tree, pretty_print=True))
         else:
             raise TypeError("Runtime version should be a string value")
 
     def _get_runtime_version_node(self):
-        return self.cpu_pkg_tree.find(f".//{{{self.ns}}}Configuration/{{{self.ns}}}AutomationRuntime")
+        return self.cpu_pkg_tree.find(
+            f".//{{{self.ns}}}Configuration/{{{self.ns}}}AutomationRuntime"
+        )
 
     def _get_build_options_node(self):
-        return self.cpu_pkg_tree.find(f".//{{{self.ns}}}Configuration/{{{self.ns}}}Build")
+        return self.cpu_pkg_tree.find(
+            f".//{{{self.ns}}}Configuration/{{{self.ns}}}Build"
+        )
 
     def _get_online_config_node(self):
-        return self.cpu_pkg_tree.find(f".//{{{self.ns}}}Configuration/{{{self.ns}}}OnlineConfiguration")
+        return self.cpu_pkg_tree.find(
+            f".//{{{self.ns}}}Configuration/{{{self.ns}}}OnlineConfiguration"
+        )
 
     def _write_to_file(self):
         self.cpu_pkg_tree.write(open(self.path, "wb"), pretty_print=True)
 
-
     # get/set runtime version
     # get/set build options:
-        # get/set additional build options
-        # get/set additional include directories
-        # get/set gcc version
-        # get/set pre build steps
-        # get/set post build steps
+    # get/set additional build options
+    # get/set additional include directories
+    # get/set gcc version
+    # get/set pre build steps
+    # get/set post build steps
     # get/set transfer options
-    
+
     def get_build_options(self):
         pass
 
@@ -68,125 +83,142 @@ class CPUPkgParser:
 
 class _CPUPkgBuildOptions:
     ns = CPUPkgParser.ns
-    def __init__(self, build_options_tree: etree.ElementTree):
+
+    def __init__(self, build_options_tree: etree.ElementTree) -> None:
         self.element = build_options_tree
 
     @property
-    def build_options(self):
+    def build_options(self) -> str:
         attrib = self.element.attrib["AnsicAdditionalBuildOptions"]
         return attrib
 
     @build_options.setter
-    def get_build_options(self, value):
-        str_val = self._params_list_to_str(value)
-        self.element.attrib["AnsicAdditionalBuildOptions"] = str_val
+    def build_options(self, value: str) -> None:
+        self.element.attrib["AnsicAdditionalBuildOptions"] = value
 
     @property
-    def gcc_version(self):
+    def gcc_version(self) -> str:
         return self.element.attrib["GccVersion"]
 
     @gcc_version.setter
-    def gcc_version(self, value):
+    def gcc_version(self, value: str) -> None:
         self.element.attrib["GccVersion"] = value
-    
+
     @property
-    def pre_build_steps(self):
-        attrib = self.element.attrib["PreBuildStep"]
-        return self._str_to_params_list(attrib)
+    def pre_build_steps(self) -> str:
+        return self.element.attrib["PreBuildStep"]
 
     @pre_build_steps.setter
-    def pre_build_steps(self, value):
-        str_val = self._params_list_to_str(value)
-        self.element.attrib["PreBuildStep"] = str_val
+    def pre_build_steps(self, value: str) -> None:
+        self.element.attrib["PreBuildStep"] = value
 
     @property
-    def post_build_steps(self):
+    def post_build_steps(self) -> str:
         return self.element.attrib["PostBuildStep"]
 
     @post_build_steps.setter
-    def post_build_steps(self, value):
-        str_val = self._str_to_params_list(self.element.attrib["PostBuildStep"])
-        return str_val
+    def post_build_steps(self, value: str) -> None:
+        self.element.attrib["PostBuildStep"] = value
 
     @property
-    def additionsl_includes(self):
-        return self.element.attrib["AnsicIncludeDirectories"]
+    def additionsl_includes(self) -> List[str]:
+        attrib = self.element.attrib["AnsicIncludeDirectories"]
+        return self._str_to_params_list(attrib)
 
     @additionsl_includes.setter
-    def additional_includes(self, value):
-        self.element.attrib["AnsicIncludeDirectories"] = value
+    def additional_includes(self, value: List[str]) -> None:
+        str_val = self._str_to_params_list(value)
+        self.element.attrib["AnsicIncludeDirectories"] = str_val
 
     @staticmethod
-    def _str_to_params_list(param):
+    def _str_to_params_list(param: str) -> List[str]:
         lst = param.split()
         return lst
 
     @staticmethod
-    def _params_list_to_str(params_list):
+    def _params_list_to_str(params_list: List[str]) -> str:
         return ", ".join(params_list)
 
 
 class _CPUPkgOnlineConfig:
     ns = CPUPkgParser.ns
-    def __init__(self, online_config_element):
+
+    def __init__(self, online_config_element: etree.Element):
         self.element = online_config_element
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.element.attrib["Name"]
 
     @name.setter
-    def name(self, value):
+    def name(self, value: str) -> None:
         self.element.attrib["Name"] = value
 
     @property
-    def device_type(self):
+    def device_type(self) -> str:
         return self.element.attrib["DeviceType"]
 
     @device_type.setter
-    def device_type(self, value):
+    def device_type(self, value: str) -> None:
         self.element.attrib["DeviceType"] = value
 
     @property
-    def device_parameters(self):
+    def device_parameters(self) -> str:
+        return self.element.attrib["DeviceParameters"]
+
+    @device_parameters.setter
+    def device_parameters(self, value: str) -> None:
+        self.element.attrib["DeviceParameters"] = value
+
+    def get_device_params_list(self) -> List[Tuple[str, str]]:
         params_str = self.element.attrib["DeviceParameters"]
         return self._param2list(params_str)
 
-    @device_parameters.setter
-    def device_parameters(self, value):
-        self.element.attrib["DeviceParameters"] = self._list_param2str(value)
+    def set_device_params_list(self, value: List[Tuple[str, str]]) -> None:
+        if isinstance(value, List):
+            self.element.attrib["DeviceParameters"] = self._list_param2str(value)
+        else:
+            raise TypeError("Connection parameters should be a list.")
 
     @property
-    def connection_parameters(self):
+    def connection_parameters(self) -> str:
+        return self.element.attrib["ConnectionParameters"]
+
+    @connection_parameters.setter
+    def connection_parameters(self, value: str) -> None:
+        self.element.attrib["ConnectionParameters"] = value
+
+    def get_connection_params_list(self) -> List[Tuple[str, str]]:
         params_str = self.element.attrib["ConnectionParameters"]
         return self._param2list(params_str)
 
-    @connection_parameters.setter
-    def connection_parameters(self, value):
-        self.element.attrib["ConnectionParameters"] = self._list_param2str(value)
+    def set_connection_params_list(self, value: List[Tuple[str, str]]) -> None:
+        if isinstance(value, List):
+            self.element.attrib["ConnectionParameters"] = self._list_param2str(value)
+        else:
+            raise TypeError("Connection parameters should be a list.")
 
-    def _param2list(self, params: str):
+    def _param2list(self, params: str) -> List[Tuple[str, str]]:
         p_str_list = params.split()
         out_params = []
         for p in p_str_list:
             out_params.append(self._param_str2tuple(p))
         return out_params
 
-    def _param_str2tuple(self, param):
+    def _param_str2tuple(self, param: str) -> Tuple[str, str]:
         param_value_lst = param.split("=")
         par = param_value_lst[0].replace("/", "")
         val = param_value_lst[1]
         return par, val
 
-    def _list_param2str(self, params):
+    def _list_param2str(self, params: List[Tuple[str, str]]) -> str:
         lst = []
         for p in params:
             lst.append(self._param_tuple2str(*p))
         return " ".join(lst)
 
-    def _param_tuple2str(self, param, val):
+    def _param_tuple2str(self, param: str, val: str) -> str:
         return f"/{param}={val}"
-
 
 
 if __name__ == "__main__":
@@ -195,4 +227,10 @@ if __name__ == "__main__":
     a = _CPUPkgBuildOptions(cpu.cpu_pkg_tree)
     r = cpu.runtime_version
     print(r)
-    cpu.runtime_version = "C4.90"
+    cpu.online_configuration.device_type
+    cpu.online_configuration.device_parameters
+    cpu.online_configuration.connection_parameters
+
+    cpu.online_configuration.set_device_params_list(
+        [("IF", "opcua"), ("LOPO", "8082"), ("SA", "2805")]
+    )
